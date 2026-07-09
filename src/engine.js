afd45e8394089e2c673f825e8dfda3b10bd0d651
MohammadHossinzehi <112985883+MohammadHossinzehi@users.noreply.github.com>
2026-07-08 20:53:09 -0400

    Add exports for various physics components

---
+++ src/engine.js
@@ -0,0 +1,7 @@
+export { Vec2 } from './vector.js';
+export { Circle, Polygon, ShapeType } from './shapes.js';
+export { RigidBody } from './rigidbody.js';
+export { World } from './world.js';
+export { SpatialHashGrid } from './broadphase.js';
+export { detectCollision } from './collision.js';
+export { resolveVelocity, correctPositions } from './solver.js';
